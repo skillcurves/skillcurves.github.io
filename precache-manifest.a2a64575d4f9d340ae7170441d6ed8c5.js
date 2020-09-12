aabffbbb59f0b651257cfc44c082ac5ef6920f0d
Roopa Madihally <roopabdr@gmail.com>
2020-09-12 09:04:53 +0530

    Updates

---
+++ precache-manifest.a2a64575d4f9d340ae7170441d6ed8c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04ea239857e8028ea840eaf4bc4cce1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff3690a55755b5dea1ed",
+    "url": "/static/css/main.c9a93b55.chunk.css"
+  },
+  {
+    "revision": "b452496748e6038690ad",
+    "url": "/static/js/2.9dbed47f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9dbed47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3690a55755b5dea1ed",
+    "url": "/static/js/main.0c4a2b8e.chunk.js"
+  },
+  {
+    "revision": "819a3dc51a9abfe40723",
+    "url": "/static/js/runtime-main.b9866fc9.js"
+  }
+]);
